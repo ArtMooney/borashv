54ac28a3390154bceb668c01a3beff4f21bfe353
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2024-11-10 18:24:53 +0100

    setup configs and fonts

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
-<!--<script setup>-->
-<!--import Navbar from "./components/Navbar.vue";-->
-<!--import Footer from "./components/Footer.vue";-->
-<!--</script>-->
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+</script>
 
 <template>
   <div
     class="flex min-h-screen flex-col flex-nowrap items-stretch justify-between"
   >
+    <div
+      class="grid-rows-auto mx-auto grid w-full max-w-screen-2xl grid-cols-1 px-4 sm:px-4"
+    >
+      <Navbar />
+    </div>
 
-    HEJ
-    <div class="w-full h-8 bg-black"></div>
-<!--    <div-->
-<!--      class="grid-rows-auto mx-auto grid w-full max-w-screen-2xl grid-cols-1 px-4 sm:px-4"-->
-<!--    >-->
-<!--      <Navbar />-->
-<!--    </div>-->
+    <router-view />
 
-<!--    <router-view />-->
-
-<!--    <div-->
-<!--      class="grid-rows-auto mx-auto grid w-full max-w-screen-2xl grid-cols-1 px-4 sm:px-4"-->
-<!--    >-->
-<!--      <Footer />-->
-<!--    </div>-->
+    <div
+      class="grid-rows-auto mx-auto grid w-full max-w-screen-2xl grid-cols-1 px-4 sm:px-4"
+    >
+      <Footer />
+    </div>
   </div>
 </template>
 
