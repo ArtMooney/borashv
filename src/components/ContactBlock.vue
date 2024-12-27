c58458c048535671077cb4f2fae449e34be206f0
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2024-12-27 18:33:42 +0100

    styling changes to contactblock

--- src/components/ContactBlock.vue
+++ src/components/ContactBlock.vue
@@ -7,7 +7,7 @@ import { location } from "ionicons/icons";
 
 <template>
   <div
-    class="relative grid min-h-[30rem] w-full items-center justify-start gap-24 px-4 py-16 md:px-8 lg:grid-cols-3"
+    class="relative grid min-h-[30rem] w-full items-center justify-center gap-24 px-4 py-16 md:px-8 lg:grid-cols-3"
   >
     <div class="absolute bottom-0 left-0 right-0 top-0 overflow-hidden">
       <img
@@ -18,13 +18,13 @@ import { location } from "ionicons/icons";
       />
     </div>
 
-    <a class="relative flex flex-col lg:items-center" href="tel:070-22777161">
+    <a class="relative flex flex-col items-center" href="tel:070-22777161">
       <ion-icon :icon="call" class="mb-4 h-20 w-20 text-white"></ion-icon>
       <div>Mobil:</div>
       <div class="underline">070-22777161</div>
     </a>
     <a
-      class="relative flex flex-col lg:items-center"
+      class="relative flex flex-col items-center"
       href="mailto:fanrik@hotmail.com?subject=Main från Borås Hemvärnsförenings hemsida"
     >
       <ion-icon :icon="mail" class="mb-4 h-20 w-20 text-white"></ion-icon>
@@ -32,7 +32,7 @@ import { location } from "ionicons/icons";
       <div class="underline">fanrik@hotmail.com</div>
     </a>
     <a
-      class="relative flex flex-col lg:items-center"
+      class="relative flex flex-col items-center"
       href="https://www.google.com/maps/place/Ålgårdsvägen+29,+506+30+Borås/@57.7344001,12.9290169,17z/data=!3m1!4b1!4m6!3m5!1s0x465aa735fb0053b9:0xd84865a2c90a12c5!8m2!3d57.7344001!4d12.9312056!16s%2Fg%2F11c4tg9myg"
       target="_blank"
     >
