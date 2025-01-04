0578c1043c709e6bec626c74f18555e6810f4f9b
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-01-04 18:03:51 +0100

    broke out some common functions

--- src/components/Member.vue
+++ src/components/Member.vue
@@ -1,6 +1,9 @@
 <script setup>
 import Button from "../elements/Button.vue";
 import Input from "../elements/Input.vue";
+import { requiredFields } from "../js/requiredFields.js";
+import { emailValidator } from "../js/emailValidator.js";
+import { formCollector } from "../js/formCollector.js";
 </script>
 
 <template>
@@ -141,8 +144,6 @@ export default {
       emailSuccessMessage: `Tack för ert meddelande! Vi återkommer till er snart!`,
       emailErrorMessage:
         "En eller flera emailadresser som ni har angett tycks inte ha ett korrekt format.",
-      emailReg:
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
       contactForm: true,
       successMessage: false,
       errorMessage: false,
@@ -159,16 +160,33 @@ export default {
     async sendForm(event) {
       event.target.disabled = true;
 
+      if (!emailValidator(event.target.form)) {
+        const savedErrorMessage = this.defaultEmailMessage;
+        this.defaultEmailMessage = this.emailErrorMessage;
+        this.errorMessage = true;
+
+        setTimeout(() => {
+          window.addEventListener(
+            "click",
+            () => {
+              this.errorMessage = false;
+              this.defaultEmailMessage = savedErrorMessage;
+            },
+            { once: true },
+          );
+        }, 500);
+      }
+
       if (
-        this.requiredFields(event.target.form) &&
-        this.emailFormatError(event.target.form)
+        requiredFields(event.target.form) &&
+        emailValidator(event.target.form)
       ) {
         const res = await fetch("/member", {
           method: "POST",
           headers: {
             Authorization: "Basic " + btoa(this.userName + ":" + this.userPass),
           },
-          body: this.formCollector(event.target.form),
+          body: formCollector(event.target.form, this.extraFields),
         });
 
         const jsonResponse = await res.json();
@@ -188,124 +206,6 @@ export default {
       } else {
         event.target.disabled = false;
       }
-    },
-
-    requiredFields(form) {
-      const inputs = form.querySelectorAll("input");
-      const textareas = form.querySelectorAll("textarea");
-      const selectors = form.querySelectorAll("select");
-      let requiredFilled = true;
-      let radioButtonNames = [];
-
-      for (const input of inputs) {
-        if (input.required) {
-          if (!input.value) requiredFilled = false;
-          if (input.type === "checkbox" && !input.checked)
-            requiredFilled = false;
-          if (input.type === "radio") radioButtonNames.push(input.dataset.name); // push to list with radiobutton groups
-        }
-      }
-
-      radioButtonNames = [...new Set(radioButtonNames)]; // removes duplicates
-
-      for (const name of radioButtonNames) {
-        let radioButtonCleared = 0;
-        for (const input of inputs) {
-          if (input.type === "radio" && input.dataset.name === name) {
-            if (input.checked) radioButtonCleared++;
-          }
-        }
-        if (!radioButtonCleared) requiredFilled = false;
-      }
-
-      for (const input of textareas) {
-        if (input.required) {
-          if (!input.value) requiredFilled = false;
-        }
-      }
-
-      for (const input of selectors) {
-        if (input.required) {
-          if (!input.value) requiredFilled = false;
-        }
-      }
-
-      return requiredFilled;
-    },
-
-    emailFormatError(form) {
-      let emailVerificationError = false;
-      const inputs = form.querySelectorAll("input");
-
-      for (const input of inputs) {
-        if (input.type === "email" && this.emailReg.test(input.value)) {
-          emailVerificationError = true;
-        }
-      }
-
-      if (!emailVerificationError) {
-        const savedErrorMessage = this.defaultEmailMessage;
-        this.defaultEmailMessage = this.emailErrorMessage;
-        this.errorMessage = true;
-
-        setTimeout(() => {
-          window.addEventListener(
-            "click",
-            () => {
-              this.errorMessage = false;
-              this.defaultEmailMessage = savedErrorMessage;
-            },
-            { once: true },
-          );
-        }, 500);
-      }
-
-      return emailVerificationError;
-    },
-
-    formCollector(form) {
-      let formData = new FormData();
-      formData.append("form-name", form.name);
-
-      for (const item of form.querySelectorAll("input")) {
-        if (item.type !== "submit") {
-          if (item.type === "file") {
-            if (item.files[0]) {
-              for (const file of item.files) {
-                formData.append(item.name, file, file.name);
-              }
-            }
-          } else if (
-            item.name !== "gdpr-confirm" &&
-            item.name !== "clientip" &&
-            item.name !== "pageuri" &&
-            item.name !== "pagename" &&
-            item.name !== "amex"
-          ) {
-            if (item.type === "checkbox") {
-              formData.append(item.name, item.checked);
-            } else if (item.type === "radio") {
-              formData.append(item.name, item.checked);
-            } else {
-              formData.append(item.name, item.value);
-            }
-          }
-        }
-      }
-
-      for (const item of form.querySelectorAll("textarea")) {
-        formData.append(item.name, item.value);
-      }
-
-      for (const item of form.querySelectorAll("select")) {
-        formData.append(item.name, item.value);
-      }
-
-      for (const [key, value] of Object.entries(this.extraFields)) {
-        formData.append(key, value);
-      }
-
-      return formData;
     },
   },
 };
