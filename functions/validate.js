e48365f4ded5b830cc5bfd91ad46cb56a6d6784b
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-01-05 21:29:55 +0100

    validate api endpoint working

--- functions/validate.js
+++ functions/validate.js
@@ -2,10 +2,6 @@ import { corsHeaders, handleCors } from "./middleware/cors.js";
 import { checkLogin } from "./middleware/check-login.js";
 import { listTables } from "./baserow/list-tables.js";
 import { listRows } from "./baserow/list-rows.js";
-import { updateRow } from "./baserow/update-row.js";
-import { sendEmail } from "./mailgun/send-email.js";
-import { messageEmailReset } from "./content/message-email-reset.js";
-import { generateUserId } from "./helpers/generate-user-id.js";
 
 export const onRequestPost = async ({ request, env, ctx }) => {
   const corsResponse = await handleCors(request, env);
@@ -20,49 +16,40 @@ export const onRequestPost = async ({ request, env, ctx }) => {
 
   const body = await request.json();
 
-  // if (!body.email || !body.pageuri) {
-  //   return new Response(JSON.stringify("error"), { headers: corsHeaders });
-  // }
-  //
-  // const schemas = await listTables(
-  //   env.BASEROW_USERNAME,
-  //   env.BASEROW_PASSWORD,
-  //   env.BASEROW_DB_ID,
-  // );
-  //
-  // const userId = schemas.find((schema) => schema.name === "CMS users")?.id;
-  //
-  // if (!userId) {
-  //   return new Response(JSON.stringify("error"), { headers: corsHeaders });
-  // }
-  //
-  // const users = await listRows(env.BASEROW_BACKEND_TOKEN, userId); // need to compare id's
-  // const user = users.results.find((user) => user.email === body.email);
-  //
-  // if (!user) {
-  //   return new Response(JSON.stringify("error"), { headers: corsHeaders });
-  // }
-  //
-  // user["reset-id"] = generateUserId(users);
-  // const saveUser = await updateRow(
-  //   env.BASEROW_BACKEND_TOKEN,
-  //   userId,
-  //   user.id,
-  //   user,
-  // );
-  //
-  // if (saveUser.error) {
-  //   return new Response(JSON.stringify("error"), { headers: corsHeaders });
-  // }
-  //
-  // const sendToEmail = await sendEmail(
-  //   env.EMAIL_FROM,
-  //   body.email,
-  //   "Change password for your account on Simple CMS",
-  //   await messageEmailReset(body.pageuri, user["reset-id"]),
-  //   env.MAILGUN_API_KEY,
-  // );
+  if (!body.validation) {
+    return new Response(JSON.stringify({ error: "no input data" }), {
+      headers: corsHeaders,
+    });
+  }
 
-  return new Response(JSON.stringify(body), { headers: corsHeaders });
+  const schemas = await listTables(
+    env.BASEROW_USERNAME,
+    env.BASEROW_PASSWORD,
+    env.BASEROW_DB_ID,
+  );
+
+  const usersTableId = schemas.find(
+    (schema) => schema.name === "CMS users",
+  )?.id;
+
+  if (!usersTableId) {
+    return new Response(JSON.stringify({ error: "error" }), {
+      headers: corsHeaders,
+    });
+  }
+
+  const user = await listRows(
+    env.BASEROW_BACKEND_TOKEN,
+    usersTableId,
+    body.validation,
+  );
+
+  if (!user || user.results.length === 0) {
+    return new Response(JSON.stringify({ error: "error validating account" }), {
+      headers: corsHeaders,
+    });
+  }
+
+  return new Response(JSON.stringify(user), { headers: corsHeaders });
   return new Response(JSON.stringify("ok"), { headers: corsHeaders });
 };
