445caa2e6542e17243802b69433f5db15bbd8ec7
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-01-12 13:26:04 +0100

    added input component

---
+++ src/components/cms/Input.vue
@@ -0,0 +1,29 @@
+<template>
+  <input
+    v-if="
+      input.name !== 'index' &&
+      getInputType(input.type) !== 'textarea' &&
+      getInputType(input.type) !== 'file' &&
+      getInputType(input.type) !== 'date' &&
+      getInputType(input.type) !== 'checkbox' &&
+      !isToFromType(input.name)
+    "
+    @click="handleInput"
+    v-model="item[input.name]"
+    :type="getInputType(input.type)"
+    class="border border-white/25 bg-[#4a4644] p-2"
+    :name="input.name"
+  />
+</template>
+
+<script>
+export default {
+  name: "Input",
+
+  props: {},
+
+  data() {
+    return {};
+  },
+};
+</script>
