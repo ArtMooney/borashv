6c20cf5db5263417d2b14c706d844f507b889b8c
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-09-12 23:17:16 +0200

    updated to correct content

--- server/content/message-contact.js
+++ server/content/message-contact.js
@@ -1,8 +1,16 @@
 export async function messageContact() {
-  return `Tack för att du kontaktat oss!
- 
-Vi har mottagit ditt formulär och kommer att behandla det så snabbt vi kan. Om du önskar komma i kontakt med oss direkt, vänligen ring oss på 033 - 72 22 400. Vi ser fram emot att hjälpa dig och återkommer till dig inom kort.
- 
-Mvh. ADinQ teamet
+  return `Hej,
+
+
+Kul att ni vill kontakta oss på Borås Hemvärnsförening! Vi har fått ert email och återkommer snart till er.
+
+
+
+Vänliga hälsningar,
+
+
+
+/ Claes Håkansson
+www.borashv.se
 `;
 }
