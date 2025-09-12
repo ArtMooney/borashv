a6f2e4e8064db15d38e9494dc9da089520dc7953
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-09-12 23:18:02 +0200

    implemented kontakta oss page for nuxt

--- app/components/Contact.vue
+++ app/components/Contact.vue
@@ -1,275 +1,147 @@
 <template>
-  <!--  <div-->
-  <!--    id="contact"-->
-  <!--    class="col-span-1 grid grow content-center items-center gap-8 px-4 py-12 md:px-8 lg:grid-cols-5"-->
-  <!--  >-->
-  <!--    <div class="lg:col-span-2">-->
-  <!--      <h4 class="text-3xl uppercase">Eller så kontaktar vi dig!</h4>-->
-  <!--      <p>Fyll i din e-mail så kontaktar vi dig</p>-->
-  <!--    </div>-->
+  <div
+    id="contact"
+    class="col-span-1 mx-auto grid w-full max-w-screen-xl grow content-center items-center gap-8 px-4 py-12 md:px-8 lg:grid-cols-5"
+  >
+    <div class="lg:col-span-2">
+      <h4 class="text-3xl uppercase">Eller så kontaktar vi dig!</h4>
+      <p>Fyll i din e-mail så kontaktar vi dig</p>
+    </div>
 
-  <!--    <div class="lg:col-span-3">-->
-  <!--      <form-->
-  <!--        v-if="contactForm"-->
-  <!--        @submit.prevent-->
-  <!--        class="grid items-center gap-4 text-white lg:grid-cols-3"-->
-  <!--        name="contact"-->
-  <!--      >-->
-  <!--        <input-->
-  <!--          name="email"-->
-  <!--          type="email"-->
-  <!--          placeholder-text="Email"-->
-  <!--          :required="true"-->
-  <!--          class="col-span-2"-->
-  <!--        />-->
+    <div class="lg:col-span-3">
+      <form
+        v-if="contactForm"
+        @submit.prevent
+        class="grid items-center gap-4 text-white lg:grid-cols-3"
+        name="contact"
+      >
+        <input
+          v-model="formData.email"
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          class="col-span-2"
+        />
 
-  <!--        <div class="hidden">-->
-  <!--          <input-->
-  <!--            name="clientip"-->
-  <!--            type="text"-->
-  <!--            placeholder-text="clientip"-->
-  <!--            :required="false"-->
-  <!--            v-model="extraFields.clientip"-->
-  <!--          />-->
+        <div class="hidden">
+          <input name="clientip" type="text" v-model="formData.clientip" />
+          <input name="pageuri" type="text" v-model="formData.pageuri" />
+          <input name="pagename" type="text" v-model="formData.pagename" />
+          <input name="amex" type="text" v-model="formData.amex" />
+        </div>
 
-  <!--          <input-->
-  <!--            name="pageuri"-->
-  <!--            type="text"-->
-  <!--            placeholder-text="pageuri"-->
-  <!--            :required="false"-->
-  <!--            v-model="extraFields.pageuri"-->
-  <!--          />-->
+        <button
+          @click="sendForm"
+          type="submit"
+          data-wait="Vänta..."
+          class="white"
+        >
+          {{ buttonText }}
+        </button>
+      </form>
 
-  <!--          <input-->
-  <!--            name="pagename"-->
-  <!--            type="text"-->
-  <!--            placeholder-text="pagename"-->
-  <!--            :required="false"-->
-  <!--            v-model="extraFields.pagename"-->
-  <!--          />-->
+      <div v-if="successMessage">
+        <div class="mt-4 bg-[#a38373] p-4 text-black">
+          {{ emailSuccessMessage }}
+        </div>
+      </div>
 
-  <!--          <input-->
-  <!--            name="amex"-->
-  <!--            type="text"-->
-  <!--            placeholder-text="amex"-->
-  <!--            :required="false"-->
-  <!--            v-model="extraFields.amex"-->
-  <!--          />-->
-  <!--        </div>-->
-
-  <!--        <button-->
-  <!--          @click="sendForm"-->
-  <!--          :text="buttonText"-->
-  <!--          link=""-->
-  <!--          hash=""-->
-  <!--          type="submit"-->
-  <!--          data-wait="Vänta..."-->
-  <!--          class="max-w-56"-->
-  <!--        ></button>-->
-  <!--      </form>-->
-
-  <!--      <div v-if="successMessage">-->
-  <!--        <div class="mt-4 bg-[#a38373] p-4 text-black">-->
-  <!--          {{ emailSuccessMessage }}-->
-  <!--        </div>-->
-  <!--      </div>-->
-
-  <!--      <div v-if="errorMessage" class="mt-4 bg-[#a38373] p-4 text-black">-->
-  <!--        <p>{{ defaultEmailMessage }}</p>-->
-  <!--      </div>-->
-  <!--    </div>-->
-  <!--  </div>-->
+      <div v-if="errorMessage" class="mt-4 bg-[#a38373] p-4 text-black">
+        <p>{{ defaultEmailMessage }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<!--<script>-->
-<!--export default {-->
-<!--  name: "Contact",-->
+<script>
+export default {
+  name: "Contact",
 
-<!--  data() {-->
-<!--    return {-->
-<!--      userName: `${import.meta.env.VITE_USERNAME}`,-->
-<!--      userPass: `${import.meta.env.VITE_USERPASS}`,-->
-<!--      extraFields: {-->
-<!--        clientip: "",-->
-<!--        pageuri: window.location.href,-->
-<!--        pagename: document.title,-->
-<!--        amex: "",-->
-<!--      },-->
-<!--      buttonText: "Skicka",-->
-<!--      defaultEmailMessage: "Något gick fel när formuläret skulle skickas.",-->
-<!--      emailSuccessMessage: `Tack för att ni kontaktar oss! Vi återkommer till er snart!`,-->
-<!--      emailErrorMessage:-->
-<!--        "En eller flera emailadresser som ni har angett tycks inte ha ett korrekt format.",-->
-<!--      emailReg:-->
-<!--        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,-->
-<!--      contactForm: true,-->
-<!--      successMessage: false,-->
-<!--      errorMessage: false,-->
-<!--    };-->
-<!--  },-->
+  data() {
+    const config = useRuntimeConfig();
 
-<!--  async created() {-->
-<!--    const res = await fetch("https://api.ipify.org?format=json");-->
-<!--    const ip = await res.json();-->
-<!--    this.extraFields.clientip = ip.ip;-->
-<!--  },-->
+    return {
+      userName: config.public.userName,
+      userPass: config.public.userPass,
+      formData: {
+        email: "",
+        clientip: "",
+        pageuri: "",
+        pagename: "",
+        amex: "",
+      },
+      buttonText: "Skicka",
+      defaultEmailMessage: "Något gick fel när formuläret skulle skickas.",
+      emailSuccessMessage: `Tack för att ni kontaktar oss! Vi återkommer till er snart!`,
+      emailErrorMessage:
+        "En eller flera emailadresser som ni har angett tycks inte ha ett korrekt format.",
+      emailReg:
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
+      contactForm: true,
+      successMessage: false,
+      errorMessage: false,
+    };
+  },
 
-<!--  methods: {-->
-<!--    async sendForm(event) {-->
-<!--      event.target.disabled = true;-->
+  async mounted() {
+    const res = await fetch("https://api.ipify.org?format=json");
+    const ip = await res.json();
+    this.formData.clientip = ip.ip;
+    this.formData.pageuri = window.location.href;
+    this.formData.pagename = document.title;
+  },
 
-<!--      if (-->
-<!--        this.requiredFields(event.target.form) &&-->
-<!--        this.emailFormatError(event.target.form)-->
-<!--      ) {-->
-<!--        const res = await fetch("/contact", {-->
-<!--          method: "POST",-->
-<!--          headers: {-->
-<!--            Authorization: "Basic " + btoa(this.userName + ":" + this.userPass),-->
-<!--          },-->
-<!--          body: this.formCollector(event.target.form),-->
-<!--        });-->
+  methods: {
+    async sendForm(event) {
+      event.target.disabled = true;
 
-<!--        const jsonResponse = await res.json();-->
+      if (
+        requiredFields(event.target.form) &&
+        emailValidator(event.target.form)
+      ) {
+        let res = null;
+        let error = null;
 
-<!--        if (jsonResponse === "error") {-->
-<!--          this.errorMessage = true;-->
-<!--        } else if (jsonResponse === "ok") {-->
-<!--          const savedText = this.buttonText;-->
-<!--          this.buttonText = event.target.dataset.wait;-->
+        try {
+          res = await $fetch("/api/kontakt", {
+            method: "POST",
+            headers: {
+              Authorization:
+                "Basic " + btoa(this.userName + ":" + this.userPass),
+            },
+            body: this.objectToFormData(this.formData),
+          });
 
-<!--          setTimeout(() => {-->
-<!--            this.contactForm = false;-->
-<!--            this.successMessage = true;-->
-<!--            this.buttonText = savedText;-->
+          const savedText = this.buttonText;
+          this.buttonText = event.target.dataset.wait;
 
-<!--            this.$router.push({-->
-<!--              hash: "#contact",-->
-<!--            });-->
-<!--          }, 1500);-->
-<!--        }-->
-<!--      } else {-->
-<!--        event.target.disabled = false;-->
-<!--      }-->
-<!--    },-->
+          setTimeout(() => {
+            this.contactForm = false;
+            this.successMessage = true;
+            this.buttonText = savedText;
 
-<!--    requiredFields(form) {-->
-<!--      const inputs = form.querySelectorAll("input");-->
-<!--      const textareas = form.querySelectorAll("textarea");-->
-<!--      const selectors = form.querySelectorAll("select");-->
-<!--      let requiredFilled = true;-->
-<!--      let radioButtonNames = [];-->
+            this.$router.push({
+              hash: "#contact",
+            });
+          }, 1500);
+        } catch (err) {
+          error = err;
+          this.errorMessage = true;
+        }
+      } else {
+        event.target.disabled = false;
+      }
+    },
 
-<!--      for (const input of inputs) {-->
-<!--        if (input.required) {-->
-<!--          if (!input.value) requiredFilled = false;-->
-<!--          if (input.type === "checkbox" && !input.checked)-->
-<!--            requiredFilled = false;-->
-<!--          if (input.type === "radio") radioButtonNames.push(input.dataset.name); // push to list with radiobutton groups-->
-<!--        }-->
-<!--      }-->
+    objectToFormData(obj) {
+      const formData = new FormData();
+      Object.keys(obj).forEach((key) => {
+        formData.append(key, obj[key]);
+      });
 
-<!--      radioButtonNames = [...new Set(radioButtonNames)]; // removes duplicates-->
-
-<!--      for (const name of radioButtonNames) {-->
-<!--        let radioButtonCleared = 0;-->
-<!--        for (const input of inputs) {-->
-<!--          if (input.type === "radio" && input.dataset.name === name) {-->
-<!--            if (input.checked) radioButtonCleared++;-->
-<!--          }-->
-<!--        }-->
-<!--        if (!radioButtonCleared) requiredFilled = false;-->
-<!--      }-->
-
-<!--      for (const input of textareas) {-->
-<!--        if (input.required) {-->
-<!--          if (!input.value) requiredFilled = false;-->
-<!--        }-->
-<!--      }-->
-
-<!--      for (const input of selectors) {-->
-<!--        if (input.required) {-->
-<!--          if (!input.value) requiredFilled = false;-->
-<!--        }-->
-<!--      }-->
-
-<!--      return requiredFilled;-->
-<!--    },-->
-
-<!--    emailFormatError(form) {-->
-<!--      let emailVerificationError = false;-->
-<!--      const inputs = form.querySelectorAll("input");-->
-
-<!--      for (const input of inputs) {-->
-<!--        if (input.type === "email" && this.emailReg.test(input.value)) {-->
-<!--          emailVerificationError = true;-->
-<!--        }-->
-<!--      }-->
-
-<!--      if (!emailVerificationError) {-->
-<!--        const savedErrorMessage = this.defaultEmailMessage;-->
-<!--        this.defaultEmailMessage = this.emailErrorMessage;-->
-<!--        this.errorMessage = true;-->
-
-<!--        setTimeout(() => {-->
-<!--          window.addEventListener(-->
-<!--            "click",-->
-<!--            () => {-->
-<!--              this.errorMessage = false;-->
-<!--              this.defaultEmailMessage = savedErrorMessage;-->
-<!--            },-->
-<!--            { once: true },-->
-<!--          );-->
-<!--        }, 500);-->
-<!--      }-->
-
-<!--      return emailVerificationError;-->
-<!--    },-->
-
-<!--    formCollector(form) {-->
-<!--      let formData = new FormData();-->
-<!--      formData.append("form-name", form.name);-->
-
-<!--      for (const item of form.querySelectorAll("input")) {-->
-<!--        if (item.type !== "submit") {-->
-<!--          if (item.type === "file") {-->
-<!--            if (item.files[0]) {-->
-<!--              for (const file of item.files) {-->
-<!--                formData.append(item.name, file, file.name);-->
-<!--              }-->
-<!--            }-->
-<!--          } else if (-->
-<!--            item.name !== "gdpr-confirm" &&-->
-<!--            item.name !== "clientip" &&-->
-<!--            item.name !== "pageuri" &&-->
-<!--            item.name !== "pagename" &&-->
-<!--            item.name !== "amex"-->
-<!--          ) {-->
-<!--            if (item.type === "checkbox") {-->
-<!--              formData.append(item.name, item.checked);-->
-<!--            } else if (item.type === "radio") {-->
-<!--              formData.append(item.name, item.checked);-->
-<!--            } else {-->
-<!--              formData.append(item.name, item.value);-->
-<!--            }-->
-<!--          }-->
-<!--        }-->
-<!--      }-->
-
-<!--      for (const item of form.querySelectorAll("textarea")) {-->
-<!--        formData.append(item.name, item.value);-->
-<!--      }-->
-
-<!--      for (const item of form.querySelectorAll("select")) {-->
-<!--        formData.append(item.name, item.value);-->
-<!--      }-->
-
-<!--      for (const [key, value] of Object.entries(this.extraFields)) {-->
-<!--        formData.append(key, value);-->
-<!--      }-->
-
-<!--      return formData;-->
-<!--    },-->
-<!--  },-->
-<!--};-->
-<!--</script>-->
+      return formData;
+    },
+  },
+};
+</script>
