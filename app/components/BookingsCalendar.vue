28740036ccf1ba511b5f823eaeff9918faeb4cff
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-09-22 21:45:23 +0200

    made v calendar implementation nicer

--- app/components/BookingsCalendar.vue
+++ app/components/BookingsCalendar.vue
@@ -3,48 +3,45 @@
     <h4 class="text-3xl uppercase">Bokningskalender</h4>
 
     <ClientOnly>
-      <div v-if="isCalendarLoaded">
-        <component
-          :is="'VCalendar'"
-          :attributes="bookings"
-          :min-date="new Date()"
-          expanded
-          show-weeknumbers
-          transparent
-          borderless
-          :class="[
-            '[&_.vc-pane-layout]:p-6',
-            '[&_.vc-pane-header-wrapper]:p-6',
-            '[&_.vc-pane-container]:rounded',
-            '[&_.vc-pane-container]:bg-[#616a5b]',
-            '[&_.vc-weekday]:!text-[#a5b698]',
-            '[&_.vc-arrow]:!text-black',
-            '[&_.vc-title]:!text-lg',
-            '[&_.vc-title]:!text-[#a5b698]',
-            '[&_.vc-header]:!mb-8',
-            '[&_.vc-weekdays]:!mb-4',
-            '[&_.vc-weekdays]:!border-b',
-            '[&_.vc-weekdays]:!border-b-[#a5b698]/25',
-            '[&_.vc-weeknumber-content]:!text-[#a5b698]/50',
-            '[&_.vc-weeknumber]:!h-full',
-            '[&_.vc-day-content]:sm:!text-lg',
-            '[&_.vc-day]:sm:!p-4',
-            '[&_.vc-highlight]:sm:!p-6',
-            '[&_.vc-disabled]:!text-black/15',
+      <VCalendar
+        :attributes="bookings"
+        :min-date="new Date()"
+        expanded
+        show-weeknumbers
+        transparent
+        borderless
+        :class="[
+          '[&_.vc-pane-layout]:p-6',
+          '[&_.vc-pane-header-wrapper]:p-6',
+          '[&_.vc-pane-container]:rounded',
+          '[&_.vc-pane-container]:bg-[#616a5b]',
+          '[&_.vc-weekday]:!text-[#a5b698]',
+          '[&_.vc-arrow]:!text-black',
+          '[&_.vc-title]:!text-lg',
+          '[&_.vc-title]:!text-[#a5b698]',
+          '[&_.vc-header]:!mb-8',
+          '[&_.vc-weekdays]:!mb-4',
+          '[&_.vc-weekdays]:!border-b',
+          '[&_.vc-weekdays]:!border-b-[#a5b698]/25',
+          '[&_.vc-weeknumber-content]:!text-[#a5b698]/50',
+          '[&_.vc-weeknumber]:!h-full',
+          '[&_.vc-day-content]:sm:!text-lg',
+          '[&_.vc-day]:sm:!p-4',
+          '[&_.vc-highlight]:sm:!p-6',
+          '[&_.vc-disabled]:!text-black/15',
 
-            '[&_.vc-green>.vc-highlight-bg-solid]:!bg-[#22c55e]',
-            '[&_.vc-blue>.vc-highlight-bg-solid]:!bg-[#16a34a]',
-            '[&_.vc-red>.vc-highlight-bg-solid]:!bg-[#15803d]',
-            '[&_.vc-purple>.vc-highlight-bg-solid]:!bg-[#84cc16]',
-            '[&_.vc-orange>.vc-highlight-bg-solid]:!bg-[#eab308]',
-            '[&_.vc-yellow>.vc-highlight-bg-solid]:!bg-[#f97316]',
-            '[&_.vc-teal>.vc-highlight-bg-solid]:!bg-[#ef4444]',
-            '[&_.vc-indigo>.vc-highlight-bg-solid]:!bg-[#8b5cf6]',
-            '[&_.vc-pink>.vc-highlight-bg-solid]:!bg-[#3b82f6]',
-            '[&_.vc-gray>.vc-highlight-bg-solid]:!bg-[#06b6d4]',
-          ]"
-        />
-      </div>
+          '[&_.vc-green>.vc-highlight-bg-solid]:!bg-[#22c55e]',
+          '[&_.vc-blue>.vc-highlight-bg-solid]:!bg-[#16a34a]',
+          '[&_.vc-red>.vc-highlight-bg-solid]:!bg-[#15803d]',
+          '[&_.vc-purple>.vc-highlight-bg-solid]:!bg-[#84cc16]',
+          '[&_.vc-orange>.vc-highlight-bg-solid]:!bg-[#eab308]',
+          '[&_.vc-yellow>.vc-highlight-bg-solid]:!bg-[#f97316]',
+          '[&_.vc-teal>.vc-highlight-bg-solid]:!bg-[#ef4444]',
+          '[&_.vc-indigo>.vc-highlight-bg-solid]:!bg-[#8b5cf6]',
+          '[&_.vc-pink>.vc-highlight-bg-solid]:!bg-[#3b82f6]',
+          '[&_.vc-gray>.vc-highlight-bg-solid]:!bg-[#06b6d4]',
+        ]"
+      />
     </ClientOnly>
 
     <div class="mt-2 flex flex-col gap-2 rounded bg-[#616a5b]/20 p-8">
@@ -66,8 +63,15 @@
 </template>
 
 <script>
+import { Calendar as VCalendar } from "v-calendar";
+import "v-calendar/style.css";
+
 export default {
   name: "Bokningskalender",
+
+  components: {
+    VCalendar,
+  },
 
   props: {
     items: {
@@ -146,16 +150,6 @@ export default {
 
       return bookings;
     },
-  },
-
-  async mounted() {
-    if (process.client) {
-      const vcalendar = await import("v-calendar");
-      await import("v-calendar/style.css");
-
-      this.$nuxt.vueApp.component("VCalendar", vcalendar.Calendar);
-      this.isCalendarLoaded = true;
-    }
   },
 };
 </script>
