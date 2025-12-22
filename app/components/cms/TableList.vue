8a5a3f1881e0193276cc41d3437099e95ce80e70
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-12-22 20:13:19 +0100

    backup function added

--- app/components/cms/TableList.vue
+++ app/components/cms/TableList.vue
@@ -43,9 +43,35 @@ export default {
     async loadSchema() {
       const table = this.cmsStore.tables[this.tableIndex];
       this.cmsStore.tableId = table.id;
-      this.cmsStore.tableType = table.tableType;
+      this.cmsStore.viewMode = table.viewMode;
+      this.cmsStore.backupRef = table.backupRef;
+
       await this.cmsStore.loadFields();
+
+      if (this.cmsStore.backupRef !== null) {
+        this.cmsStore.tableId = this.cmsStore.backupRef;
+        await this.cmsStore.loadRows("asc", "sortOrder");
+
+        const backupItems = [];
+        for (const item of this.cmsStore.items) {
+          if (item?.date?.[1] && new Date(item.date[1]) < new Date()) {
+            backupItems.push(item);
+          }
+        }
+
+        this.cmsStore.tableId = table.id;
+        this.cmsStore.items = backupItems;
+        await this.cmsStore.saveAllItems();
+
+        this.cmsStore.tableId = this.cmsStore.backupRef;
+        await this.cmsStore.deleteAllItems();
+      }
+
       await this.cmsStore.loadRows("asc", "sortOrder");
+
+      // if (this.cmsStore.viewMode === "graph") {
+      // console.log("VIEWMODE", this.cmsStore.viewMode);
+      // }
     },
   },
 
