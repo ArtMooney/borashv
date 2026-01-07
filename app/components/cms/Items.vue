da9dbb0e5be48eb1fadf14e9caad1476233ac50e
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2026-01-07 15:26:08 +0100

    support for static pages based on json content

--- app/components/cms/Items.vue
+++ app/components/cms/Items.vue
@@ -46,12 +46,57 @@ import { VueDraggableNext } from "vue-draggable-next";
           <div class="my-4 h-px w-full bg-white/25"></div>
 
           <template v-for="input of cmsStore.schema">
-            <CmsInputs
-              v-if="input.name !== 'index'"
-              :input="input"
-              :item="item"
-              :index="index"
-            />
+            <template
+              v-if="input.type === 'json'"
+              v-for="(section, sectionKey) in staticContentTypes[item.title]"
+              :key="sectionKey"
+            >
+              <template v-for="(field, fieldKey) in section" :key="fieldKey">
+                <template v-if="typeof field === 'object'">
+                  <template
+                    v-for="(nestedField, nestedKey) in field"
+                    :key="nestedKey"
+                  >
+                    <CmsInputs
+                      :input="
+                        contentInput(
+                          input,
+                          nestedKey,
+                          nestedField,
+                          `${sectionKey}.${fieldKey}`,
+                        )
+                      "
+                      :item="
+                        getContentSection(
+                          item,
+                          input.name,
+                          sectionKey,
+                          fieldKey,
+                        )
+                      "
+                      :index="index"
+                    />
+                  </template>
+                </template>
+
+                <template v-else>
+                  <CmsInputs
+                    :input="contentInput(input, fieldKey, field, sectionKey)"
+                    :item="getContentSection(item, input.name, sectionKey)"
+                    :index="index"
+                  />
+                </template>
+              </template>
+            </template>
+
+            <template v-else>
+              <CmsInputs
+                v-if="input.name !== 'index'"
+                :input="input"
+                :item="item"
+                :index="index"
+              />
+            </template>
           </template>
 
           <button type="submit" class="hidden"></button>
@@ -81,7 +126,7 @@ import { VueDraggableNext } from "vue-draggable-next";
 <script>
 import { useLoginStore } from "~/components/cms/stores/loginStore";
 import { useCmsStore } from "~/components/cms/stores/cmsStore";
-import { graphConfig } from "~/../server/db/schema";
+import { graphConfig, staticContentTypes } from "~/../server/db/schema";
 
 export default {
   name: "CmsItems",
@@ -127,6 +172,45 @@ export default {
 
     deepClone(obj) {
       return JSON.parse(JSON.stringify(obj));
+    },
+
+    contentInput(input, fieldKey, field, sectionKey) {
+      return {
+        ...input,
+        name: fieldKey,
+        label: sectionKey + "." + fieldKey,
+        type: field,
+      };
+    },
+
+    getContentSection(item, inputName, ...pathKeys) {
+      if (!item[inputName]) {
+        item[inputName] = {};
+      }
+
+      let current = item[inputName];
+      let template = staticContentTypes[item.title];
+
+      for (let i = 0; i < pathKeys.length; i++) {
+        const key = pathKeys[i];
+        template = template?.[key];
+
+        if (!current[key]) {
+          const isLastKey = i === pathKeys.length - 1;
+
+          if (isLastKey && template && typeof template === "object") {
+            current[key] = Object.fromEntries(
+              Object.keys(template).map((k) => [k, ""]),
+            );
+          } else {
+            current[key] = {};
+          }
+        }
+
+        current = current[key];
+      }
+
+      return current;
     },
   },
 
